ed759c8dd7c8d1ba4159e312bbb5d91564153444
NafisMahmudAyon <nafismahmudayon@gmail.com>
2022-11-05 20:45:11 +0600

    C Programming

---
+++ C Problem 1- C Program To Find Area And Circumference Of Circle/Problem 1.c
@@ -0,0 +1,24 @@
+// Problem 1
+// QUE: C Program To Find Area And Circumference Of Circle.
+
+// Source Code:
+
+#includestdio.h
+
+main()
+{
+   // Follow US on = [ C Programming ]
+  
+   int rad;
+   float PI = 3.14, area, ci;
+
+   printf(nEnter radius of circle );
+   scanf(%d, &rad);
+
+   area = PI  rad  rad;
+   printf(nArea of circle  %f , area);
+
+   ci = 2  PI  rad;
+   printf(nCircumference  %f , ci);
+
+}
---
+++ C Problem 2- C Program To Print ASCII Value Of Character/C Problem 2.c
@@ -0,0 +1,21 @@
+// Problem 1:
+// QUE: C Program To Find Area And Circumference Of Circle.
+
+// Source Code:
+
+#include<stdio.h>
+
+main()
+{
+   // Follow US on =>> [ C Programming ]
+  
+   char a;
+   int b;
+
+   printf("Enter The Character ");
+   scanf("%c",&a);
+
+   b=a;
+   printf("\nOutput Is =%d ",b);
+
+}
